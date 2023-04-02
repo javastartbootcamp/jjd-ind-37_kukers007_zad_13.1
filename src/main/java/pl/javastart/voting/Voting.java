13a4c15d0c70caf17f9ffe80237ccc0b40a8bede
kukers007 <96911087+kukers007@users.noreply.github.com>
2023-04-02 21:58:00 +0200

    Solution 13.1

--- src/main/java/pl/javastart/voting/Voting.java
+++ src/main/java/pl/javastart/voting/Voting.java
@@ -26,9 +26,45 @@ public class Voting {
      * do wczytywania danych od użytkownika użyj scannera z parametru
      * Metoda powinna pobrać głos dla każdego przekazanego głosującego i zapisać wyniki głosowania do VotingResult
      */
+
     VotingResult executeVoting(List<String> voters, Scanner scanner) {
+        List<Boolean> votes = new ArrayList<>();
+        for (String voter : voters) {
+            System.out.println("Jak głosuje " + voter + "? (z - za, p - przeciw, w - wstrzymanie się)");
+            String input = scanner.nextLine().toLowerCase();
+            while (!input.equals("z") && !input.equals("p") && !input.equals("w")) {
+                System.out.println("Niepoprawna wartość! Jak głosuje " + voter + "? (z - za, p - przeciw, w - wstrzymanie się)");
+                input = scanner.nextLine().toLowerCase();
+            }
 
-        return null; // to możesz (a nawet powinieneś/powinnaś) zmienić :)
+            if (input.equals("z")) {
+                votes.add(true);
+            } else if (input.equals("p")) {
+                votes.add(false);
+            } else {
+                votes.add(null);
+            }
+        }
+
+        int forCount = 0;
+        int againstCount = 0;
+        int abstainedCount = 0;
+
+        for (Boolean vote : votes) {
+            if (vote == null) {
+                abstainedCount++;
+            } else if (vote) {
+                forCount++;
+            } else {
+                againstCount++;
+            }
+        }
+
+        float forPercentage = (float) forCount / votes.size() * 100;
+        float againstPercentage = (float) againstCount / votes.size() * 100;
+        float abstainedPercentage = (float) abstainedCount / votes.size() * 100;
+
+        return new VotingResult(forPercentage, againstPercentage, abstainedPercentage);
     }
-
+    // to możesz (a nawet powinieneś/powinnaś) zmienić :)
 }
--- src/main/java/pl/javastart/voting/VotingResult.java
+++ src/main/java/pl/javastart/voting/VotingResult.java
@@ -4,6 +4,40 @@ package pl.javastart.voting;
  * Możesz dodać kolejne metody i pola do klasy. Nie zmieniaj istniejących metod.
  */
 public class VotingResult {
+    float percentVotesFor;
+    float percentVotesAgainst;
+    float percentAbstentions;
+
+    public VotingResult(float percentVotesFor, float percentVotesAgainst, float percentAbstentions) {
+        this.percentVotesFor = percentVotesFor;
+        this.percentVotesAgainst = percentVotesAgainst;
+        this.percentAbstentions = percentAbstentions;
+
+    }
+
+    public float getPercentVotesFor() {
+        return percentVotesFor;
+    }
+
+    public void setPercentVotesFor(float percentVotesFor) {
+        this.percentVotesFor = percentVotesFor;
+    }
+
+    public float getPercentVotesAgainst() {
+        return percentVotesAgainst;
+    }
+
+    public void setPercentVotesAgainst(float percentVotesAgainst) {
+        this.percentVotesAgainst = percentVotesAgainst;
+    }
+
+    public float getPercentAbstentions() {
+        return percentAbstentions;
+    }
+
+    public void setPercentAbstentions(float percentAbstentions) {
+        this.percentAbstentions = percentAbstentions;
+    }
 
     /**
      * Metoda powinna drukować wyniki głosowania w takiej postaci:
@@ -12,6 +46,9 @@ public class VotingResult {
      * Wstrzymało się: 8.47%
      */
     public void printResults() {
+        System.out.printf("Głosów za: %.2f%%\n", percentVotesFor);
+        System.out.printf("Głosów przeciw: %.2f%%\n", percentVotesAgainst);
+        System.out.printf("Wstrzymało się: %.2f%%\n", percentAbstentions);
         // metoda powinna drukować wyniki głosowania
     }
 
@@ -22,6 +59,6 @@ public class VotingResult {
      * Nie zmieniaj sygnatury tej metody!
      */
     public void printVoteForVoter(String voterName) {
-
+        System.out.println(voterName);
     }
 }
